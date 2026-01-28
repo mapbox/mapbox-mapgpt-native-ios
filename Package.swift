e4587a0abf89b40db8abb05f01cf66e9ed7faaff
Tatiana Ian <tatiana.ian@mapbox.com>
2026-01-28 11:43:02 +0200

    [release] v23.12.0-alpha.2

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 import Foundation
 
-let commonVersion : Version = "24.14.0"
-let mapGptVersion = "24.14.0"
-let mapGptChecksum = "84fb0f2fbca4ade96bf3b4a6d00503f7f4ea0471bcf937c8e1d07c1fb6a16c0f"
+let commonVersion : Version = "23.12.0-alpha.2"
+let mapGptVersion = "23.12.0-alpha.2"
+let mapGptChecksum = "fe9b20a80289708927be1de873f9672a3928a4063838b0fea81d266625b8139a"
 let releaseType = "releases"
 
 let package = Package(
     name: "MapboxMapGpt",
-    platforms: [.iOS(.v12), .macOS(.v10_15), .custom("visionos", versionString: "1.0")],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .custom("visionos", versionString: "1.0")],
     products: [
         .library(name: "MapboxMapGpt", targets: ["MapboxMapGptWrapper"]),
     ],
